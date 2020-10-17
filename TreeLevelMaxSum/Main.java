412cd33a7d6ec27c964d40d4a2a254904b44e167
thudsonbu <hudson_thom@bentley.edu>
2020-10-16 20:20:45 -0400

    First problem solution

--- TreeLevelMaxSum/Main.java
+++ TreeLevelMaxSum/Main.java
@@ -1,21 +1,54 @@
-package treeLevelMaxSum;
-
 import java.util.HashMap;
 
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         
-        Node root = new Node( 5, null, null);
+        Node root = new Node( 5, null, null );
 
-        int highestSum = root.buildRandomTree();
+        double highestSum = root.buildRandomTree();
 
-        if ( root.value > highestSum ) { 
-            highestSum = root.value;
+        if ( root.value > highestSum ) highestSum = root.value;
+
+        assert highestSum == highestLevelSum( root ) : "Error";
+
+        System.out.println(highestSum);
+
+    }
+
+    public static double highestLevelSum( Node root ) {
+
+        HashMap<Double,Double> levelSumMap = new HashMap<Double,Double>();
+
+        highestLevelSumHelper( root, levelSumMap, 0);
+
+        double maxSum = Double.NEGATIVE_INFINITY;
+
+        double currentLevel = 0;
+
+        while ( levelSumMap.containsKey(currentLevel) ) {
+            double levelSum = levelSumMap.get(currentLevel);
+            if ( levelSum > maxSum ) {
+
+                maxSum = levelSum;
+            }
         }
 
-        
-        
+        return maxSum;
+    }
 
+    public static void highestLevelSumHelper( Node root, HashMap<Double,Double> map, double level ){
+
+        double currentValue = map.getOrDefault( level, 0.0 );
+
+        map.put( level, currentValue + root.value );
+
+        if ( root.left != null ) {
+            highestLevelSumHelper( root.left, map, level+1 );
+        }
+
+        if ( root.right != null ) { 
+            highestLevelSumHelper( root.right, map, level+1 );
+        }
 
     }
 }
--- TreeLevelMaxSum/Node.java
+++ TreeLevelMaxSum/Node.java
@@ -1,25 +1,22 @@
-package treeLevelMaxSum;
-
-
 public class Node {
     // Instance variables
-    public int value;
+    public double value;
     public Node left;
     public Node right;
 
     // Constructor
-    public Node( int value, Node left, Node right) {
+    public Node( double value, Node left, Node right) {
         this.value = value;
         this.left = left;
         this.right = right;
     }
 
-    public int buildRandomTree(){
+    public double buildRandomTree(){
 
-        int largestRowSum = 0;
+        double largestRowSum = 0;
 
-        int[] level1 = randArr(2);
-        int[] level2 = randArr(4);
+        double[] level1 = randArr(2);
+        double[] level2 = randArr(4);
 
         // check which row has higher sum (this will be used in testing)
         if ( arrSum(level1) > arrSum(level2) ) {
@@ -44,23 +41,23 @@ public class Node {
         return largestRowSum;
     }
 
-    public static int arrSum(int[] level) {
+    public static double arrSum(double[] level) {
         
-        int sum = 0;
+        double sum = 0;
 
-        for (int num : level){
+        for (double num : level){
             sum += num;
         }
 
         return sum;
     }
 
-    public static int[] randArr(int length) {
+    public static double[] randArr(int length) {
 
-        int[] arr = new int[length];
+        double[] arr = new double[length];
 
         for (int i = 0; i < length; i++) {
-            int nextNum = (int) Math.random() * 100;
+            double nextNum = Math.random() * 100;
             arr[i] = nextNum;
         }
 
