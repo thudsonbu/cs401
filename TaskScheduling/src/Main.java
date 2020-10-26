266aca4471548a8c408ba10a709bcc5bb324c9a3
thudsonbu <hudson_thom@bentley.edu>
2020-10-26 09:28:03 -0400

    Switch to intellij project

---
+++ TaskScheduling/src/Main.java
@@ -0,0 +1,71 @@
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        String[] tasks = {"a", "b", "a", "c", "d", "c", "a"};
+
+
+
+    }
+
+    public static String[] scheduleTasks(String[] tasks, int downTime){
+
+        ArrayList<String> scheduledTasks = new ArrayList<>();
+
+        HashMap<String, Integer> taskCountMap = readTasksToMap(tasks);
+
+        PriorityQueue<Task> queue = readTasksMapToQueue(taskCountMap);
+
+        ArrayList<Task> runningTasks = new ArrayList<>();
+
+        for(String task : tasks){
+
+            Task maxTask = queue.poll();
+            scheduledTasks.add(maxTask.name);
+
+            for(int i = 0; i < downTime; i++){
+
+                Task nextTask = queue.poll();
+
+                for (Task runningTask : runningTasks){
+
+                }
+            }
+        }
+
+        return tasks;
+    }
+
+    public static HashMap<String, Integer> readTasksToMap(String[] arr){
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(String task : arr){
+
+            if(map.containsKey(task)){
+
+                int currentCount = map.get(task);
+
+                map.put(task, currentCount + 1);
+            } else {
+
+                map.put(task, 1);
+            }
+        }
+
+        return map;
+    }
+
+    public static PriorityQueue<Task> readTasksMapToQueue(HashMap<String, Integer> map){
+
+        PriorityQueue<Task> taskQueue = new PriorityQueue<Task>(100);
+
+        map.forEach( (String key, Integer value) -> { taskQueue.add(new Task(key, value)); });
+
+        return taskQueue;
+    }
+}
---
+++ TaskScheduling/src/Task.java
@@ -0,0 +1,47 @@
+public class Task implements Comparable<Task> {
+
+    String name;
+    int count;
+
+    public Task(String name, int count){
+
+        this.name = name;
+        this.count = count;
+    }
+
+    public int compareTo(Task compTask){
+
+        if (compTask.count < this.count){
+
+            return -1;
+        } else {
+
+            return 1;
+        }
+    }
+
+    public boolean equals(Object otherObject) {
+
+        if (otherObject == null){
+
+            return false;
+
+        } else if(this.getClass() != otherObject.getClass()){
+
+            return false;
+
+        } else {
+
+            Task otherTask = (Task)otherObject;
+
+            if (otherTask.name.equals(this.name)){
+
+                return true;
+
+            } else {
+
+                return false;
+            }
+        }
+    }
+}
