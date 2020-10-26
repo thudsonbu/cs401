f8fcb36007d8e8f68eea53dd8b0823b6d8f571ad
thudsonbu <hudson_thom@bentley.edu>
2020-10-26 11:27:35 -0400

    Task schedule works but non optimal solution

--- TaskScheduling/src/Main.java
+++ TaskScheduling/src/Main.java
@@ -6,38 +6,65 @@ public class Main {
 
     public static void main(String[] args){
 
-        String[] tasks = {"a", "b", "a", "c", "d", "c", "a"};
+        String[] tasks = {"a", "b", "a", "c", "d", "c", "a", "c"};
 
+        ArrayList<String> taskSchedule = scheduleTasks(tasks, 2);
 
+        for(String task : taskSchedule){
 
+            System.out.print(task + ", ");
+        }
     }
 
-    public static String[] scheduleTasks(String[] tasks, int downTime){
+    public static ArrayList<String> scheduleTasks(String[] tasks, int downTime){
 
         ArrayList<String> scheduledTasks = new ArrayList<>();
 
-        HashMap<String, Integer> taskCountMap = readTasksToMap(tasks);
+        HashMap<String, Integer> taskCountMap = readTasksToMap(tasks); // keep track of tasks and remaining runs
 
-        PriorityQueue<Task> queue = readTasksMapToQueue(taskCountMap);
+        PriorityQueue<Task> queue = readTasksMapToQueue(taskCountMap); // keep track of which tasks to schedule next
 
-        ArrayList<Task> runningTasks = new ArrayList<>();
+        ArrayList<Task> runningTasks = new ArrayList<>(); // keep track of running tasks and wait
 
         for(String task : tasks){
 
-            Task maxTask = queue.poll();
-            scheduledTasks.add(maxTask.name);
-
-            for(int i = 0; i < downTime; i++){
+            // if there are tasks in the queue schedule them and add them to running
+            if(queue.size() > 0){
 
                 Task nextTask = queue.poll();
 
-                for (Task runningTask : runningTasks){
+                int remaining = taskCountMap.get(nextTask.name);
 
+                taskCountMap.put(nextTask.name, remaining - 1);
+
+                runningTasks.add(new Task(nextTask.name, downTime));
+
+                scheduledTasks.add(nextTask.name);
+
+            } else {
+
+                scheduledTasks.add("none");
+            }
+
+            // re add to queue tasks which have "cooled down" and decrement wait
+            for(Task runningTask : runningTasks){
+
+                if(runningTask.count == 0){ // task should be added back if no remaining wait
+
+                    int count = taskCountMap.get(runningTask.name); // get how many remaining runs
+
+                    if(count > 0){ // only add back if task has remaining runs
+
+                        queue.add(new Task(runningTask.name, count));
+                    }
+                } else {
+
+                    runningTask.count--; // decrement remaining wait
                 }
             }
         }
 
-        return tasks;
+        return scheduledTasks;
     }
 
     public static HashMap<String, Integer> readTasksToMap(String[] arr){
@@ -62,9 +89,9 @@ public class Main {
 
     public static PriorityQueue<Task> readTasksMapToQueue(HashMap<String, Integer> map){
 
-        PriorityQueue<Task> taskQueue = new PriorityQueue<Task>(100);
+        PriorityQueue<Task> taskQueue = new PriorityQueue<>(100);
 
-        map.forEach( (String key, Integer value) -> { taskQueue.add(new Task(key, value)); });
+        map.forEach( (String key, Integer value) -> taskQueue.add(new Task(key, value)));
 
         return taskQueue;
     }
